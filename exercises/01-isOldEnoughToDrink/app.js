cdeea4e0f6ecd5fd58490200f0ec9119377bc650
amafjarkasi <amafjarkasi@gmail.com>
2020-09-30 20:31:15 +0000

    latest updates

--- exercises/01-isOldEnoughToDrink/app.js
+++ exercises/01-isOldEnoughToDrink/app.js
@@ -1,3 +1,13 @@
-function isOldEnoughToDrink(){
+function isOldEnoughToDrink(age){
     // your code here	
+    return (age >= 21 ? true : false);
 }
+
+var output = isOldEnoughToDrink(22);
+console.log(output);
+
+console.log(isOldEnoughToDrink(21));
+
+console.log(isOldEnoughToDrink(19));
+
+
--- exercises/02-isOldEnoughToDrive/app.js
+++ exercises/02-isOldEnoughToDrive/app.js
@@ -1,3 +1,10 @@
 function isOldEnoughToDrive(age) {
   // your code here
+  return (age >= 16 ? true : false);
 }
+
+console.log(isOldEnoughToDrive(22));
+console.log(isOldEnoughToDrive(16));
+console.log(isOldEnoughToDrive(14));
+
+
